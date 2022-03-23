834fa97fc77c984ed003dc7f8bbf84bcfa1beb2b
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-03-23 20:52:50 +0530

    Added an error

--- src/errors.py
+++ src/errors.py
@@ -1,6 +1,11 @@
 class NoCallBack:
-	def __init__(self, msg):
-		self.msg = msg
+	pass
 
-	def __str__(self):
-		return self.msg
+class ResponseError:
+	def __init__(self, status_code:int, response, expected_status_code:int=200):
+		self.status_code = status_code
+		self.response = response
+		self.expected_status_code = expected_status_code
+	
+	def __str__(self) -> str:
+		return f"The server responded with a {self.status_code} status code." + ("" if self.expected_status_code == 200 else f" Expected {self.expected_status_code}")
